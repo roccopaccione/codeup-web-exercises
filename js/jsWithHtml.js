98ed2bab9de92e494f0023459d627bd428b282a9
Rocco  Paccione <leonardo.rocco.paccione@gmail.com>
2020-08-10 15:10:33 -0500

    practice with Js and HTML

---
+++ js/jsWithHtml.js
@@ -0,0 +1,20 @@
+"use strict";
+
+console.log('Hello from JS with HTML');
+
+var whatIsUp = "What's Up!!";
+alert(whatIsUp);
+
+var question = "Do you like Pizza?";
+confirm(question);
+
+
+var question2 = "What is your favorite city?";
+prompt(question2);
+console.log(question2);
+
+var question3 = "How old are you?";
+prompt(question3);
+console.log(question3);
+console.log(typeof question3);
+
