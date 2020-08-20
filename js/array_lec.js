0a14b18fc5b484efdb2e4ffe4a1fff67053efa55
Rocco  Paccione <leonardo.rocco.paccione@gmail.com>
2020-08-20 10:17:16 -0500

    Array lecture practice

---
+++ js/array_lec.js
@@ -0,0 +1,9 @@
+var daysOfTheWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+var today = daysOfTheWeek[3];
+console.log('Today is ' + today + '.');
+
+console.log(daysOfTheWeek);
+
+for (i = 0; i < daysOfTheWeek.length; i++) {
+    console.log(daysOfTheWeek[i]);
+}
