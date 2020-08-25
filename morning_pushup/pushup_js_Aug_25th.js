ea727cc0fc6c4a7d1ac68bfa88da5f2681f40dcd
Rocco  Paccione <leonardo.rocco.paccione@gmail.com>
2020-08-25 09:21:49 -0500

    morning pushup finished

---
+++ morning_pushup/pushup_js_Aug_25th.js
@@ -0,0 +1,15 @@
+function pushup(){
+    for (i=1;i <= 100;i++) {
+        if(i%5 === 0 && i%3 === 0) {
+            console.log(i + " FIZZ, BUZZ")
+        }else if(i%5 === 0) {
+            console.log(i + " BUZZ");
+        }else if(i%3 === 0){
+            console.log(i + " FIZZ");
+        }else{
+            console.log(i);
+        }
+    }
+}
+
+console.log(pushup());
