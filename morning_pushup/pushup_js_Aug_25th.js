b68a2a5cf6d75938971428147394b0a1f5aafd0f
Rocco  Paccione <leonardo.rocco.paccione@gmail.com>
2020-08-25 10:37:20 -0500

    morning pushup and BOM lec  practice

--- morning_pushup/pushup_js_Aug_25th.js
+++ morning_pushup/pushup_js_Aug_25th.js
@@ -1,15 +1,34 @@
-function pushup(){
-    for (i=1;i <= 100;i++) {
-        if(i%5 === 0 && i%3 === 0) {
-            console.log(i + " FIZZ, BUZZ")
-        }else if(i%5 === 0) {
-            console.log(i + " BUZZ");
-        }else if(i%3 === 0){
-            console.log(i + " FIZZ");
-        }else{
-            console.log(i);
-        }
-    }
-}
+'use strict';
+// function pushup(){
+//     for (i=1;i <= 100;i++) {
+//         if(i%5 === 0 && i%3 === 0) {
+//             console.log(i + " FIZZ, BUZZ")
+//         }else if(i%5 === 0) {
+//             console.log(i + " BUZZ");
+//         }else if(i%3 === 0){
+//             console.log(i + " FIZZ");
+//         }else{
+//             console.log(i);
+//         }
+//     }
+// }
+//
+// console.log(pushup());
+//
+// var num = 0;
+// var max = 10;
+// var interval = 2000;
+//
+// var intervalFor = setInterval(function () {
+//     if (num >= max) {
+//         clearInterval(intervalFor);
+//         alert("Done");
+//     }else{
+//         num++;
+//         alert("iterating on line " + num + ".");
+//     }
+// }, interval);
+//
+// console.log(intervalFor);
+//
 
-console.log(pushup());
