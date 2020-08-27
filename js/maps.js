04bd19e56f91dd77f0f9fce4e36c554e96b2df2c
Rocco  Paccione <leonardo.rocco.paccione@gmail.com>
2020-08-27 13:50:37 -0500

    finished mapbox practices

--- js/maps.js
+++ js/maps.js
@@ -1,1 +1,11 @@
 console.log(secret);
+
+mapboxgl.accessToken = MAPBOX_TOKEN;
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/satellite-streets-v11', // stylesheet location
+        center: [-98.489615, 29.426827], // starting position [lng, lat]
+        zoom: 20 // starting zoom
+});
+
+var jeddah = new mapboxgl.Marker().setLngLat([39.0829, 21.7340]).addTo(map);
